e5f7266724d5bb802856b6f2d73e061768bdecc9
Sabbir Ahmed <262800523+monotrixdev@users.noreply.github.com>
2026-05-08 19:04:47 +0600

    okey

--- app/api/truecaller/route.ts
+++ app/api/truecaller/route.ts
@@ -1,8 +1,4 @@
-import { headers } from 'next/headers';
-import { authClient } from "@/lib/auth-client";
-import { Look } from "@/lib/lookup";
 import { NextRequest, NextResponse } from "next/server";
-import { auth } from '@/lib/auth';
 
 function formatPhoneNumber(phoneNumber: string) {
   let formattedNumber = phoneNumber.replace(/\D/g, "");
@@ -37,17 +33,6 @@ function detectCarrier(number: string) {
 }
 
 export async function GET(req: NextRequest) {
-
-  const result = await auth.api.getSession({
-    headers: await headers()
-  });
-  if (!result?.session) {
-    return NextResponse.json(
-      { error: "Unauthorized" },
-      { status: 401 }
-    );
-  }
-
   const start = performance.now();
 
   const number = req.nextUrl.searchParams.get("number");
@@ -91,10 +76,6 @@ export async function GET(req: NextRequest) {
     const carrier = detectCarrier(cli);
 
     if (nameMatch && typeMatch) {
-      await Look.create({
-        userId: result?.user?.id,
-        phoneNumber: cli,
-      });
       return NextResponse.json({
         status: true,
         responseTime: `${responseTime}s`,
@@ -114,7 +95,6 @@ export async function GET(req: NextRequest) {
       carrier,
     });
   } catch (error) {
-    console.error("Error fetching from Truecaller API:", error);
     return NextResponse.json(
       {
         status: false,
